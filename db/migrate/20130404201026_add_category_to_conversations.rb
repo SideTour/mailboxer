fd593b5c93c8bababa066a0563bb063932dba60d
SideTour Admin <admin@sidetour.com>
2013-04-04 17:21:16 -0400

    Fix typo in index

--- db/migrate/20130404201026_add_category_to_conversations.rb
+++ db/migrate/20130404201026_add_category_to_conversations.rb
@@ -1,6 +1,6 @@
 class AddCategoryToConversations < ActiveRecord::Migration
   def change
     add_column :conversations, :conversation_type, :string, default: ""
-    add_index :conversation, :conversation_type
+    add_index :conversations, :conversation_type
   end
 end
