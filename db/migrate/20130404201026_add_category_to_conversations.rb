8ee62afde677f8104679b94dcccffb77b95e8abe
SideTour Admin <admin@sidetour.com>
2013-04-04 16:59:23 -0400

    Add index to conversation category

--- db/migrate/20130404201026_add_category_to_conversations.rb
+++ db/migrate/20130404201026_add_category_to_conversations.rb
@@ -1,5 +1,6 @@
 class AddCategoryToConversations < ActiveRecord::Migration
   def change
     add_column :conversations, :category, :string, default: ""
+    add_index :conversation, :category
   end
 end
